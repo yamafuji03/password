b9d1786fed35a44d97cd40551c902302903f6ee1
aa <yamafuji03@gmail.com>
2024-09-17 15:22:58 +0900

    add password追加

--- text.sh
+++ text.sh
@@ -3,6 +3,12 @@
 FILE="passwords.txt"
 
 echo "パスワードマネージャーへようこそ！"
+echo "次の選択肢から入力してください (add password/get password/exit):"
+read -r choice
+
+# add passwrord
+if [ "$choice" == "add password" ]; then
+
 
 # サービス名、ユーザー名、パスワードの入力を求める
 read -p "サービス名を入力してください：" service
@@ -10,7 +16,10 @@ read -p "ユーザー名を入力してください：" username
 read -p "パスワードを入力してください：" password
 
 # 入力情報をファイルに保存
-echo "$service:$username:$password" >> "$FILE"
+echo "サービス:$service ユーザー名:$username: パスワード:$password" >> "$FILE"
 
 # 完了メッセージ
 echo "Thank you!"
+
+
+fi
