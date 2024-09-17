ae4b404e08571c13857ff62b78f5fbc2a2704763
aa <yamafuji03@gmail.com>
2024-09-17 15:37:26 +0900

    get password以外できた

--- text.sh
+++ text.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # パスワードを保存するファイルの名前
 FILE="passwords.txt"
@@ -6,20 +7,26 @@ echo "パスワードマネージャーへようこそ！"
 echo "次の選択肢から入力してください (add password/get password/exit):"
 read -r choice
 
-# add passwrord
-if [ "$choice" == "add password" ]; then
+# `choice` の値に応じた処理を実行
+if [ "$choice" = "add password" ]; then
+    # サービス名、ユーザー名、パスワードの入力を求める
+    read -p "サービス名を入力してください：" service
+    read -p "ユーザー名を入力してください：" username
+    read -p "パスワードを入力してください：" password
+
+    # 入力情報をファイルに保存
+    echo "サービス:$service ユーザー名:$username パスワード:$password" >> "$FILE"
+
+    # 完了メッセージ
+    echo "Thank you!"
+
+elif [ "$choice" = "get password" ]; then
+    echo "途中"
 
 
-# サービス名、ユーザー名、パスワードの入力を求める
-read -p "サービス名を入力してください：" service
-read -p "ユーザー名を入力してください：" username
-read -p "パスワードを入力してください：" password
 
-# 入力情報をファイルに保存
-echo "サービス:$service ユーザー名:$username: パスワード:$password" >> "$FILE"
-
-# 完了メッセージ
-echo "Thank you!"
-
-
+elif [ "$choice" = "exit" ]; then
+    echo "終わります"
+else
+    echo "入力が変です"
 fi
