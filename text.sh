bbb7c516245ae34b7f9bac380d7ff18871c00de7
aa <yamafuji03@gmail.com>
2024-09-17 14:55:12 +0900

    step1

--- text.sh
+++ text.sh
@@ -1,14 +1,16 @@
+
+# パスワードを保存するファイルの名前
+FILE="passwords.txt"
+
 echo "パスワードマネージャーへようこそ！"
 
-
-# サービス名の入力
-read -p "サービス名を入力してください：" service_name
-
-# ユーザー名の入力
+# サービス名、ユーザー名、パスワードの入力を求める
+read -p "サービス名を入力してください：" service
 read -p "ユーザー名を入力してください：" username
-
-# パスワードの入力（入力が見えないようにする）
 read -p "パスワードを入力してください：" password
 
+# 入力情報をファイルに保存
+echo "$service:$username:$password" >> "$FILE"
 
+# 完了メッセージ
 echo "Thank you!"
